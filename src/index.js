14214b5e9bee437eebbca3480da0dddac36f6a50
zonive <jparalusz@edu.cdv.pl>
2022-01-10 17:18:12 +0100

    08 - REST to CRUD

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import config from "config";
+
+import databaseProvider from "./DatabaseProvider.js";
 import Server from "./Server.js";
 import routes from "./routes/index.js";
 
-const server = new Server(
-  config.cors_options,
-  config.options.public_routes_prefix,
-  config.options.private_routes_prefix
-);
-server.addRoutes(routes);
-server.start(
-  config.options.port,
-  config.security.key_path,
-  config.security.cert_path
-);
+const run = async () => {
+  try {
+    await databaseProvider.testConnection();
 
-server.addRoutes();
+    const server = new Server(
+      config.cors_options,
+      config.options.public_routes_prefix,
+      config.options.private_routes_prefix
+    );
+
+    server.addRoutes(routes);
+
+    server.start(
+      config.options.port,
+      config.security.key_path,
+      config.security.cert_path
+    );
+
+    server.addRoutes();
+  } catch (err) {
+    console.error(err);
+    // eslint-disable-next-line no-undef
+    process.exit(1);
+  }
+};
+
+run();
