a84b86922ebbb5306406390f429dc67d363c0cf2
zonive <jparalusz@edu.cdv.pl>
2022-01-10 16:40:23 +0100

    07 - Tests

---
+++ config/test.cjs
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    is_test: true,
+    name: "restaurant-manager-js-test",
+    options: {
+      port: 3001,
+    },
+  };
---
+++ tests/routes/status.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import { callApi } from "../helpers.js";
+
+const PATH = `api/public/status`;
+
+test(`[GET] ${PATH} - valid request - should return status 200`, async () => {
+  const response = await callApi("get", "/status", {}, {}, true);
+  expect(response.status).toEqual(200);
+  expect(response.data?.status).toEqual("OK");
+});
